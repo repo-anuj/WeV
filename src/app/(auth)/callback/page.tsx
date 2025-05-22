d4cd4cc87c39811f50700d2f00474657d0bd955f
repo-anuj <anujd4348@gmail.com>
2025-05-22 16:53:28 +0530

    0...

---
+++ src/app/(auth)/callback/page.tsx
@@ -0,0 +1,8 @@
+//TODO: implement the auth callback page
+export const dynamic = "force-dynamic";
+
+const AuthCallbackPage = async () => {
+    return <div>AuthCallbackPage</div>;
+}
+
+export default AuthCallbackPage;
